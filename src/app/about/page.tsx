84693f728f784909838b9115b2b72628d1ed5ebc
tranminhwang <j2team.tranminhquang@gmail.com>
2023-07-19 17:38:26 +0700

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 function About() {
   return (
-    <div>
-      <h1>Hello from S3</h1>
-    </div>
+    <main className="min-h-screen p-24">
+      <h1>Hello from About Page</h1>
+      <p>
+        This is project is a template for a Next.js app that is deployed to AWS
+        S3.
+      </p>
+      <button className="px-4 py-2 rounded-lg bg-blue-500 text-white">
+        <Link href="/">Go to Home Page</Link>
+      </button>
+    </main>
   );
 }
 
