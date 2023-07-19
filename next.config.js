500af9a3a81439da326d38a453f6f2a94295a488
tranminhwang <j2team.tranminhquang@gmail.com>
2023-07-19 16:46:43 +0700

    deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  distDir: "build",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
